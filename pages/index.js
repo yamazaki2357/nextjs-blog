00b442845874eb5c253fb5c24065af5b05c5e9fc
yamazaki2357 <yamazaki2357@gmail.com>
2023-04-09 18:11:08 +0900

    Navigate Between Pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -10,9 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-          こんにちはーーー！
+        <h1 className="title">
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
